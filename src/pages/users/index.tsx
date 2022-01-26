838f79385b9ff8f6be1230abd6d7f2785133ff5d
Thiago Padovani <padovanitr@gmail.com>
2022-01-26 10:05:15 -0300

    feat: add responsiviness to pagination and users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
@@ -6,6 +6,11 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList(){
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Box>
             <Header />
@@ -31,17 +36,17 @@ export default function UserList(){
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4","4","6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink"/>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                 { isWideVersion && <Th>Data de cadastro</Th> }
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4","4","6"]}>
                                     <Checkbox colorScheme="pink"/>
                                 </Td>
                                 <Td>
@@ -50,16 +55,17 @@ export default function UserList(){
                                         <Text fontSize="sm" color="gray.300">padovanitr@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>10 de Janeiro, 2021</Td>
+                                { isWideVersion && <Td>10 de Janeiro, 2021</Td>}
                                 <Td>
                                     <Button
                                         as="a"
                                         size="sm"
                                         fontSize="sm"
                                         colorScheme="purple"
+                                        iconSpacing={isWideVersion ? '1.5' : '-0.5'}
                                         leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}                        
                                     >
-                                        Editar
+                                        { isWideVersion && 'Editar' }
                                     </Button>
                                 </Td>
                             </Tr>
