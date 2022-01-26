d66f9b8fa58320745991762d45e1255e98542e7c
Thiago Padovani <padovanitr@gmail.com>
2022-01-25 23:36:32 -0300

    feat: sidebar context to open em close on mobile

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile(){
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps){
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Thiago Padovani</Text>
-                <Text color="gray.300" fontSize="small">
-                    padovanitr@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && 
+                <Box mr="4" textAlign="right">
+                    <Text>Thiago Padovani</Text>
+                    <Text color="gray.300" fontSize="small">
+                        padovanitr@gmail.com
+                    </Text>
+                </Box>
+            }
 
             <Avatar size="md" name="Thiago Padovani" src="https://github.com/padovanitr.png"/>
         </Flex>
