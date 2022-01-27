aff2fcde3907658a00ca6c55984c39a54574894e
Thiago Padovani <padovanitr@gmail.com>
2022-01-27 16:45:18 -0300

    feat: finish active link

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -3,15 +3,22 @@ import { useRouter } from 'next/router';
 import { cloneElement, ReactElement } from 'react';
 
 interface ActiveLinkProps extends LinkProps {
-    children: ReactElement
+    children: ReactElement;
+    shouldMatchExactHref?: boolean;
 }
  
-export function ActiveLink({ children, ...rest }: ActiveLinkProps){
+export function ActiveLink({ children, shouldMatchExactHref = false, ...rest }: ActiveLinkProps){
     const { asPath } = useRouter();
     let isActive = false;
 
-    if (asPath === rest.href || asPath === rest.as){
+    if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
         isActive = true;
+    }
+
+    if (!shouldMatchExactHref && 
+        (asPath.startsWith(String(rest.href)) || 
+        asPath.startsWith(String(rest.as)))) {
+        isActive = true;  
     }
 
     return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,7 +16,7 @@ export default function UserList(){
         <Box>
             <Header />
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" p={["","8"]}>
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px={["","6"]}>
                 <Sidebar />
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="6">
